d04bc824a2dbc728265a43ceecbdf14bb0981e63
maxhelsel <46327674+maxhelsel@users.noreply.github.com>
2022-02-01 23:57:50 -0500

    paint

--- src/components/anchors.js
+++ src/components/anchors.js
@@ -4,6 +4,15 @@ import PropTypes from 'prop-types'
 export default class Anchors extends React.PureComponent {
   constructor(props) {
     super(props)
+
+    let defaultCategory = props.categories.filter(
+      (category) => category.first,
+    )[0]
+
+    this.state = {
+      selected: defaultCategory.name,
+    }
+
     this.handleClick = this.handleClick.bind(this)
   }
 
@@ -15,16 +24,21 @@ export default class Anchors extends React.PureComponent {
   }
 
   render() {
-    var { categories, color, i18n, icons } = this.props
+    var { categories, color, i18n, icons } = this.props,
+      { selected } = this.state
 
     return (
-      <nav className="emoji-mart-anchors" aria-label={i18n.categorieslabel} >
+      <nav className="emoji-mart-anchors" aria-label={i18n.categorieslabel}>
         {categories.map((category, i) => {
-          var { id, name, anchor } = category
+          var { id, name, anchor } = category,
+            isSelected = name == selected
+
           if (anchor === false) {
             return null
           }
+
           const iconId = id.startsWith('custom-') ? 'custom' : id
+
           return (
             <button
               key={id}
@@ -35,7 +49,7 @@ export default class Anchors extends React.PureComponent {
               onClick={this.handleClick}
               className={`emoji-mart-anchor`}
             >
-              <div className="emoji-mart-anchor-icon" >
+              <div className="emoji-mart-anchor-icon">
                 {icons.categories[iconId]()}
               </div>
             </button>
